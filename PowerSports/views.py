554857ede34aa237393b3ca494a0be74316c0724
Alexandru Chetran <myg.alle26@gmail.com>
2023-12-11 19:39:33 +0000

    urls revision

--- PowerSports/views.py
+++ PowerSports/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
-
+from django.shortcuts import render
 
 # Create your views here.
 
