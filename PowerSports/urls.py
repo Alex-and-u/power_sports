5fe4d92f06bfd62792d549830173c370397a9555
larisaparv <larisasova97@gmail.com>
2023-12-11 21:11:32 +0200

    created urls for each view

--- PowerSports/urls.py
+++ PowerSports/urls.py
@@ -3,5 +3,10 @@ from .views import power_sports
 
 urlpatterns = [
     path('',power_sports, name='ps'),
-    path('about/',power_sports, name='ps'),
+    path('about/',power_sports, name='about'),
+    path('blog/', power_sports, name='blog'),
+    path('contact/', power_sports, name='contact'),
+    path('news/', power_sports, name='news'),
+    path('single_blog/', power_sports, name='single_blog'),
+    path('team/', power_sports, name='team'),
 ]
--- PowerSports/views.py
+++ PowerSports/views.py
@@ -15,3 +15,33 @@ def about(request):
     html_template = loader.get_template('about.html')
     context = {}
     return HttpResponse(html_template.render(context, request))
+
+
+def blog(request):
+    html_template = loader.get_template('blog.html')
+    context = {}
+    return HttpResponse(html_template.render(context, request))
+
+
+def contact(request):
+    html_template = loader.get_template('contact.html')
+    context = {}
+    return HttpResponse(html_template.render(context, request))
+
+
+def news(request):
+    html_template = loader.get_template('news.html')
+    context = {}
+    return HttpResponse(html_template.render(context, request))
+
+
+def single_blog(request):
+    html_template = loader.get_template('single-blog.html')
+    context = {}
+    return HttpResponse(html_template.render(context, request))
+
+
+def team(request):
+    html_template = loader.get_template('team.html')
+    context = {}
+    return HttpResponse(html_template.render(context, request))
