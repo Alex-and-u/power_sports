0f1d3801782d424497f24304857d148e00b57d57
Alexandru Chetran <myg.alle26@gmail.com>
2023-12-13 17:06:14 +0000

    models classes creation

--- PowerSports/models.py
+++ PowerSports/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    surname = models.CharField(max_length=50, null=False)
+    username = models.CharField(max_length=50, null=False)
+    player_id = models.DecimalField(max_length=10, primary_key=True, null=True)
+
+
+
+class Field(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    address = models.CharField(max_length=50, null=False)
+    field_id = models.DecimalField(max_length=10, primary_key=True, null=True)
+
+
+
+
+
+class Sport(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    surname = models.CharField(max_length=50, null=False)
+    sport_id = models.DecimalField(max_length=10, primary_key=True, null=True)
